22c1d3948b2488b3cd5ecd243b8e204ee38825bf
Roey Borsteinas <roeyb1@hotmail.com>
2020-09-12 15:46:38 -0400

    Implemented ITickable interface

--- ScarletEngine/include/Core/Engine.h
+++ ScarletEngine/include/Core/Engine.h
@@ -1,7 +1,12 @@
 #pragma once
 
+#include <vector>
+#include <mutex>
+
 namespace ScarletEngine
 {
+	class ITickable;
+
 	class Engine
 	{
 	public:
@@ -10,21 +15,51 @@ namespace ScarletEngine
 
 		void SignalQuit() { bIsRunning = false; }
 
-		void Update(double DeltaTime) const;
-		void FixedUpdate(double DeltaTime) const;
+		void Update(double DeltaTime);
+		void FixedUpdate(double DeltaTime);
+
+		bool IsInitialized() { return bIsInitialized; }
+
+		/** Enqueue the addition of a new tickable object for the next frame */
+		void QueueAddTickable(ITickable* TickableObject);
+		/** Remove a tickable object. This must be immediate as it will only be called on destruction of an ITickable */
+		void RemoveTickable(ITickable* TickableObject);
 
 		/** Return a reference to the global Engine object */
 		static Engine& Get() { static Engine Instance; return Instance; }
 	private:
-        void Terminate();
+		void Terminate();
 
+		/** Called before objects are ticked each frame */
+		void PreUpdate();
+		/** Called after objects are ticked each frame */
+		void PostUpdate();
+		
+		/** Moves queued tickables into the list of objects to tick on the next frame */
+		void AddQueuedTickables();
+		void AddTickable(ITickable* TickableToAdd);
 	private:
 		// Prevent instantiations of this class and disable copy/move constructors
-		Engine() = default;
+		Engine();
 		Engine(const Engine&) = delete;
 		Engine(Engine&&) = delete;
+		
+		/** List of tickables to update on a fixed-timestep */
+		std::vector<ITickable*> VariableUpdateTickables;
+		/** List of tickables to update on a variable-timestep */
+		std::vector<ITickable*> FixedUpdateTickables;
+		/** Queue of tickables to add at the beginning of the next frame */
+		std::vector<ITickable*> TickableQueue;
+		/** Used to lock the TickableQueue */
+		std::mutex TickableQueueMutex;
 
-		bool bIsInitialized = false;
-		bool bIsRunning = false;
+		/** True when the engine is initialized */
+		uint32_t bIsInitialized : 1;
+		/** True when the engine is in the running state */
+		uint32_t bIsRunning : 1;
+		/** True after the engine has finished running */
+		uint32_t bIsTerminated : 1;
+		/** True when the engine is in the middle of ticking objects */
+		uint32_t bTickingObjects : 1;
 	};
 }
--- ScarletEngine/src/Core/Engine.cpp
+++ ScarletEngine/src/Core/Engine.cpp
@@ -1,13 +1,27 @@
 #include "Core/Engine.h"
 
 #include "Core/CoreUtils.h"
+#include "Core/ITickable.h"
 
 #include <chrono>
+#include <algorithm>
 
 #define FIXED_UPDATE_MS 20.0
 
 namespace ScarletEngine
 {
+	Engine::Engine()
+		: VariableUpdateTickables()
+		, FixedUpdateTickables()
+		, TickableQueue()
+		, bIsInitialized(false)
+		, bIsRunning(false)
+		, bIsTerminated(false)
+		, bTickingObjects(false)
+	{
+
+	}
+
 	void Engine::Initialize()
 	{
 		Logger::Get().SetLogFile("Log.txt");
@@ -32,32 +46,118 @@ namespace ScarletEngine
 			LastTime = Clock.now();
 			Lag += DeltaTime;
 
-			// If it has been more than FIXED_UPDATE_MS since our last fixed timestep update,
-			// we should run a fixed update before running a regular update
-			while (Lag >= FIXED_UPDATE_MS)
+			PreUpdate();
+			// In this scope we are ticking objects
 			{
-				Lag -= FIXED_UPDATE_MS;
-				FixedUpdate(FIXED_UPDATE_MS);
+				bTickingObjects = true;
+				// If it has been more than FIXED_UPDATE_MS since our last fixed timestep update,
+				// we should run a fixed update before running a regular update
+				while (Lag >= FIXED_UPDATE_MS)
+				{
+					Lag -= FIXED_UPDATE_MS;
+					FixedUpdate(FIXED_UPDATE_MS);
+				}
+
+				Update(DeltaTime);
+				bTickingObjects = false;
 			}
 
-			Update(DeltaTime);
+			// Process anything that should happen before the next update
+			PostUpdate();
 		}
 
 		Terminate();
 	}
 
+	void Engine::PreUpdate()
+	{
+		AddQueuedTickables();
+	}
+
+	void Engine::PostUpdate()
+	{
+
+	}
+
 	void Engine::Terminate()
 	{
+		bIsInitialized = false;
+		VariableUpdateTickables.clear();
+		FixedUpdateTickables.clear();
 
+		bIsTerminated = true;
 	}
 
-	void Engine::Update(double) const
+	void Engine::Update(double DeltaTime)
 	{
-
+		for (const auto Tickable : VariableUpdateTickables)
+		{
+			Tickable->Tick(DeltaTime);
+		}
 	}
 
-	void Engine::FixedUpdate(double) const
+	void Engine::FixedUpdate(double DeltaTime)
 	{
+		for (const auto Tickable : FixedUpdateTickables)
+		{
+			Tickable->Tick(DeltaTime);
+		}
+	}
 
+	void Engine::QueueAddTickable(ITickable* TickableObject)
+	{
+		check(bIsInitialized);
+		std::lock_guard<std::mutex> Lock(TickableQueueMutex);
+
+		TickableQueue.push_back(TickableObject);
+	}
+
+	void Engine::RemoveTickable(ITickable* TickableObject)
+	{
+		// Tickables cannot be removed while objects are being ticked as this would invalidate the iterator
+		check(!bTickingObjects);
+
+		if (TickableObject->WantsFixedTimestep())
+		{
+			auto It = std::remove(FixedUpdateTickables.begin(), FixedUpdateTickables.end(), TickableObject);
+			if (It != FixedUpdateTickables.end())
+			{
+				FixedUpdateTickables.erase(It, FixedUpdateTickables.end());
+			}
+		}
+		else
+		{
+			auto It = std::remove(VariableUpdateTickables.begin(), VariableUpdateTickables.end(), TickableObject);
+			if (It != VariableUpdateTickables.end())
+			{
+				VariableUpdateTickables.erase(It, VariableUpdateTickables.end());
+			}
+		}
+	}
+
+	void Engine::AddQueuedTickables()
+	{
+		std::lock_guard<std::mutex> Lock(TickableQueueMutex);
+		// Add any new tickables which may have been enqueued during the last frame
+		for (const auto TickableToAdd : TickableQueue)
+		{
+			AddTickable(TickableToAdd);
+		}
+		TickableQueue.clear();
+	}
+
+	void Engine::AddTickable(ITickable* TickableObject)
+	{
+		check(bIsInitialized);
+		check(!bTickingObjects);
+
+		if (TickableObject->WantsFixedTimestep())
+		{
+			FixedUpdateTickables.push_back(TickableObject);
+		}
+		else
+		{
+			VariableUpdateTickables.push_back(TickableObject);
+		}
 	}
 }
