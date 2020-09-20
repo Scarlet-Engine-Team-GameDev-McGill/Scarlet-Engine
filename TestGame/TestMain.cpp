dbd2d670059cb832bd139ff9065bed89df488ec1
Roey Borsteinas <roeyb1@hotmail.com>
2020-09-20 14:33:25 -0400

    Implemented basic serialization object

--- TestGame/TestMain.cpp
+++ TestGame/TestMain.cpp
@@ -33,59 +33,25 @@ struct Velocity
 int main()
 {
 	using namespace ScarletEngine;
+	Archive Arch;
+
+	for (int i = 0; i < 20; ++i)
 	{
-		Archive Arch;
-
-		for (int i = 0; i < 20; ++i)
-		{
-			Position Pos{ (float)i, (float)(i + 1), (float)(i + 2) };
-			Arch << Pos;
-		}
-
-		Arch.SaveToFile("TestFile.data");
-
-		Array<Position> Values;
-
-		Archive InArch("TestFile.data");
-
-		for (int i = 0; i < 20; ++i)
-		{
-			Position Pos;
-			InArch >> Pos;
-			Values.push_back(Pos);
-		}
-
-		Values.clear();
+		Position Pos{ (float)i, (float)(i + 1), (float)(i + 2) };
+		Arch << Pos;
 	}
-	{
-		Array<Position> SerializedArray;
-		for (int i = 0; i < 20; ++i)
-		{
-			SerializedArray.push_back({ (float)(i / 1.f), (float)(i / 2.f), (float)(i / 3.f) });
-		}
-		Archive ArrayArch;
-		ArrayArch << SerializedArray;
-		ArrayArch.SaveToFile("ArrayFile.data");
 
-		Archive InArrayArch("ArrayFile.data");
-		Array<Position> InArray;
-		InArrayArch >> InArray;
-	}
-	{
-		Array<String> SerializedStringArray;
-		for (int i = 0; i < 20; ++i)
-		{
-			char Buff[32];
-			snprintf(Buff, 32, "Hello world %d", i);
-			SerializedStringArray.emplace_back(Buff);
-		}
-		Archive StringArch;
-		StringArch << SerializedStringArray;
-		StringArch.SaveToFile("StringFile.data");
+	Arch.SaveToFile("TestFile.data");
 
-		Archive InStringArch("StringFile.data");
-		Array<String> InStringArray;
-		InStringArch >> InStringArray;
+	Array<Position> Values;
+
+	Archive InArch("TestFile.data");
+
+	for (int i = 0; i < 20; ++i)
+	{
+		Position Pos;
+		InArch >> Pos;
+		Values.push_back(Pos);
 	}
 	{
 		Archive NestedArrayArch;
@@ -100,15 +66,20 @@ int main()
 		NestedArrayArch << NestedArray;
 		NestedArrayArch.SaveToFile("NestedArray.data");
 
-
-		Array<Array<int>> InNestedArray;
-		Archive InNestedArrayArch("NestedArray.data");
-		InNestedArrayArch >> InNestedArray;
-		
-		InNestedArray.clear();
-	}
-
+	Values.clear();
 	
+	Array<Position> SerializedArray;
+	for (int i = 0; i < 20; ++i)
+	{
+		SerializedArray.push_back({ (float)(i / 1.f), (float)(i / 2.f), (float)(i / 3.f) });
+	}
+	Archive ArrayArch;
+	ArrayArch << SerializedArray;
+	ArrayArch.SaveToFile("ArrayFile.data");
+
+	Archive InArrayArch("ArrayFile.data");
+	Array<Position> InArray;
+	InArrayArch >> InArray;
 
 	return 0;
 }
