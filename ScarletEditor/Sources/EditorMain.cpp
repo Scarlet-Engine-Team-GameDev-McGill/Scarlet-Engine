b9caf9d0f122ca9f94ebcf9db0ea4b5617ff1e7e
RobinLmn <55120018+RobinLmn@users.noreply.github.com>
2021-02-11 16:04:37 -0500

    Corrected ECS architecture for RigidBody implementation

--- ScarletEditor/Sources/EditorMain.cpp
+++ ScarletEditor/Sources/EditorMain.cpp
@@ -3,13 +3,39 @@
 #include "Editor.h"
 #include "RAL.h"
 #include "StaticMeshComponent.h"
+#include "Ac_RigidBodyComponent.h"
 #include "AssetManager.h"
 #include "RenderModule.h"
 #include "UIModule.h"
 
+using namespace ScarletEngine;
+
+void makeCube(glm::vec3 Pos)
+{
+	auto [Ent, Trans, Mesh, Rb] = GEditor->GetActiveWorld()->CreateEntity<Transform, StaticMeshComponent, Ac_RigidBodyComponent>("Cube");
+
+	Trans->Position = Pos;
+	Trans->Rotation = glm::vec3(45.f, 45.f, 0.f);
+	Trans->Scale = glm::vec3(0.05f);
+
+	Rb->Mass = 1.f;
+
+	Mesh->MeshHandle = AssetManager::LoadStaticMesh("/ScarletEngine/Content/Cube.obj");
+	Mesh->VertexBuff = RAL::Get().CreateBuffer((uint32_t)Mesh->MeshHandle->Vertices.size() * sizeof(Vertex), RALBufferUsage::STATIC_DRAW);
+	Mesh->VertexBuff->UploadData(Mesh->MeshHandle->Vertices.data(), Mesh->MeshHandle->Vertices.size() * sizeof(Vertex));
+	Mesh->IndexBuff = RAL::Get().CreateBuffer((uint32_t)Mesh->MeshHandle->Indices.size() * sizeof(uint32_t), RALBufferUsage::STATIC_DRAW);
+	Mesh->IndexBuff->UploadData(Mesh->MeshHandle->Indices.data(), Mesh->MeshHandle->Indices.size() * sizeof(uint32_t));
+	Mesh->VertexArray = RAL::Get().CreateVertexArray(Mesh->VertexBuff, Mesh->IndexBuff);
+
+	auto VertShader = RAL::Get().CreateShader(RALShaderStage::Vertex, "/ScarletEngine/Shaders/test_shader.vert");
+	auto FragShader = RAL::Get().CreateShader(RALShaderStage::Pixel, "/ScarletEngine/Shaders/test_shader.frag");
+	Mesh->Shader = RAL::Get().CreateShaderProgram(VertShader, FragShader, nullptr, nullptr);
+	GlobalAllocator<RALShader>::Free(VertShader);
+	GlobalAllocator<RALShader>::Free(FragShader);
+};
+
 int main()
 {
-	using namespace ScarletEngine;
 	
 	// #todo_Core: this should be loaded by a config file or something, for now default it to this.
 	AssetManager::SetAssetRoot("../");
@@ -27,26 +53,8 @@ int main()
 
 	{
 		// Test entity
-		auto [Ent, Mesh, Trans, Rb] = GEditor->GetActiveWorld()->CreateEntity<Transform, StaticMeshComponent, Ac_RigidBodyComponent>("Cube");
-
-		Trans->Position = glm::vec3(0.f, 0.5f, 0.f);
-		Trans->Rotation = glm::vec3(0.f, 0.f, 0.f);
-		Trans->Scale = glm::vec3(0.5f);
-
-		Rb->SetMass(1.f);
-
-		Mesh->MeshHandle = AssetManager::LoadStaticMesh("/ScarletEngine/Content/Cube.obj");
-		Mesh->VertexBuff = RAL::Get().CreateBuffer((uint32_t)Mesh->MeshHandle->Vertices.size() * sizeof(Vertex), RALBufferUsage::STATIC_DRAW);
-		Mesh->VertexBuff->UploadData(Mesh->MeshHandle->Vertices.data(), Mesh->MeshHandle->Vertices.size() * sizeof(Vertex));
-		Mesh->IndexBuff = RAL::Get().CreateBuffer((uint32_t)Mesh->MeshHandle->Indices.size() * sizeof(uint32_t), RALBufferUsage::STATIC_DRAW);
-		Mesh->IndexBuff->UploadData(Mesh->MeshHandle->Indices.data(), Mesh->MeshHandle->Indices.size() * sizeof(uint32_t));
-		Mesh->VertexArray = RAL::Get().CreateVertexArray(Mesh->VertexBuff, Mesh->IndexBuff);
-
-		auto VertShader = RAL::Get().CreateShader(RALShaderStage::Vertex, "/ScarletEngine/Shaders/test_shader.vert");
-		auto FragShader = RAL::Get().CreateShader(RALShaderStage::Pixel, "/ScarletEngine/Shaders/test_shader.frag");
-		Mesh->Shader = RAL::Get().CreateShaderProgram(VertShader, FragShader, nullptr, nullptr);
-		ScarDelete(VertShader);
-		ScarDelete(FragShader);
+		makeCube(glm::vec3(-10.f, 0.f, 0.f));
+		makeCube(glm::vec3(10.f, 0.f, 0.f));
 	}
 
 	GEngine->Run();
