338de6c595cf664cbaac536bc7928c2e6059636b
Roey Borsteinas <roeyb1@hotmail.com>
2021-02-12 10:39:34 -0500

    Changed OpenGL context version to 4.5
    Not necessary to have the absolute latest version.

--- ScarletEngine/Sources/OpenGLRAL/Private/OpenGLRAL.cpp
+++ ScarletEngine/Sources/OpenGLRAL/Private/OpenGLRAL.cpp
@@ -82,7 +82,7 @@ namespace ScarletEngine
 		ZoneScoped
 		glfwInit();
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
 		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 		glfwWindowHint(GLFW_SAMPLES, 4);
 #ifdef DEBUG
