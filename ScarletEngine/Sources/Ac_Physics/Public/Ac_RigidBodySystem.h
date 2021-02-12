61a4cffdf5a0d1b84a008d9e318573117c464117
RobinLmn <55120018+RobinLmn@users.noreply.github.com>
2021-02-11 19:23:48 -0500

    Implemented physically based gravitational simulation

--- ScarletEngine/Sources/Ac_Physics/Public/Ac_RigidBodySystem.h
+++ ScarletEngine/Sources/Ac_Physics/Public/Ac_RigidBodySystem.h
@@ -12,6 +12,8 @@ namespace ScarletEngine
 	public:
 		Ac_RigidBodySystem(Registry* InReg, const String& InName);
 		static void AddForce(const glm::vec3 aForce, Ac_RigidBodyComponent* aComponent);
+		static void ResetForce(Ac_RigidBodyComponent* Rb);
+		void ComputeGravities(const Array<SharedPtr<Entity>>& Entities) const;
 		virtual void UpdateEntity(EID EntityID, double DeltaTime) const override;
 		virtual void Update(const Array<SharedPtr<Entity>>& Entities, double DeltaTime) const override;
 	};
